c3cdf6d161037e7985a6935dce638f9ee2d35c87
christian060301 <christian.mreyes06@gmail.com>
2023-02-08 19:38:07 -0600

    nombre del proyecto: my-app. Descripcion:version final de proyecto my-app

---
+++ src/main/java/com/chris/myapp/entity/TestEntity.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.chris.myapp.entity;
+
+/**
+ * @author Christian
+ *
+ */
+public class TestEntity {
+	/*
+	 * Propiedad de prueba para la muestra de compilacion del archivo testentity
+	 * 
+	 */
+	
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	private String nombre;
+	
+}
